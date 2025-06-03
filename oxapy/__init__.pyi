8cabeb6a8ebdb084a88edcbf36b4442e5616f291
FITAHIANA Nomeniavo joe <24nomeniavo@gmail.com>
2025-06-03 15:31:24 +0300

    add pyi file

---
+++ oxapy/__init__.pyi
@@ -0,0 +1,40 @@
+from oxapy import *
+
+__all__ = [
+    "HttpServer",
+    "Router",
+    "Status",
+    "Response",
+    "Request",
+    "Cors",
+    "get",
+    "post",
+    "delete",
+    "patch",
+    "put",
+    "head",
+    "options",
+    "static_file",
+    "catcher",
+    "templating",
+    "serializer",
+]
+
+# Names in __all__ with no definition:
+#   Cors
+#   HttpServer
+#   Request
+#   Response
+#   Router
+#   Status
+#   catcher
+#   delete
+#   get
+#   head
+#   options
+#   patch
+#   post
+#   put
+#   serializer
+#   static_file
+#   templating
