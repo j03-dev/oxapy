30351016cb9a324eb43d3bcaaddf7b6ffd60227e
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-08-02 12:53:45 +0300

    chore: make test sync

--- tests/app.py
+++ tests/app.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from oxapy import Oxapy, Router, get
 
 
@@ -8,13 +6,12 @@ def greet(_r, name: str):
     return f"Hello, {name}!"
 
 
-async def main():
+def main():
     oxapy = Oxapy(("0.0.0.0", 5555))
     oxapy.set_patterns(["*.py", "*.html"])
     oxapy.attach(Router().route(greet).route(get("/health", lambda _: "Good")))
-    oxapy.async_mode()
-    await oxapy.run(reload=True)
+    oxapy.run(reload=True)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
