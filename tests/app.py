4638179742d4195214c81ca31231848acd96f193
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-08-11 18:43:36 +0300

    chore: modify the example test

--- tests/app.py
+++ tests/app.py
@@ -1,14 +1,13 @@
 from oxapy import Oxapy, Router, get
 
 
+@get("/hello/{name}")
+def hello(_req, name):
+    return f"Hello, {name}!"
+
+
 def main():
-    (
-        Oxapy(("127.0.0.1", 5555))
-        .attach(
-            Router().route(get("/greet/{name}", lambda _, name: f"Hello, {name}!"))
-        )
-        .run()
-    )
+    Oxapy(("127.0.0.1", 5555)).attach(Router().route(hello)).run()
 
 
 if __name__ == "__main__":
