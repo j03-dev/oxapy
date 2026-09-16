bd4e65052af2dca073219c946c16348ffd79fb4d
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-09-16 12:54:09 +0300

    feat: staticly check the params of handlers

--- tests/app.py
+++ tests/app.py
@@ -2,14 +2,21 @@ import multiprocessing
 workers = multiprocessing.cpu_count()
 
 
-from oxapy import Oxapy, Router, get, post
+from oxapy import Oxapy, Request, Router, get, post
 
 
+@get("/hello/{name:str}")
+def hello(r: Request):
+    return "Hello, {name}"
+
+
+ 
 def main():
     (
         Oxapy(("0.0.0.0", 3000))
         .attach(
             Router()
+            .route(hello)
             .route(get("/", lambda _: ""))
             .route(get("/user/{id:int}", lambda _, id: str(id)))
             .route(post("/user", lambda _: ""))
