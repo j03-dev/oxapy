b6011938e6eef0e88acb7d4f1f9d905b9fa261fd
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-09-16 21:41:20 +0300

    chore: remove hello from test

--- tests/app.py
+++ tests/app.py
@@ -5,18 +5,11 @@ workers = multiprocessing.cpu_count()
 from oxapy import Oxapy, Request, Router, get, post
 
 
-@get("/hello/{name:str}")
-def hello(r: Request, name: str):
-    return "Hello, {name}"
-
-
- 
 def main():
     (
         Oxapy(("0.0.0.0", 3000))
         .attach(
             Router()
-            .route(hello)
             .route(get("/", lambda _: ""))
             .route(get("/user/{id:int}", lambda _, id: str(id)))
             .route(post("/user", lambda _: ""))
